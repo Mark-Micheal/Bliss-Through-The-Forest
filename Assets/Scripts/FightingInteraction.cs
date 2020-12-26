84034b61d03e01171fa3f7c0e069da01d3c463fb
Mark-Micheal <markmichealhp@outlook.com>
2020-12-26 02:03:49 +0200

    Situation 1  started

---
+++ Assets/Scripts/FightingInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightingInteraction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collider Triggered");
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Collider Player");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Collider Exited");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player Left");
+        }
+    }
+}
