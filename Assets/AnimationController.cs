1d95f9bdf88a489d339f97faeb248832a7968634
Mark-Micheal <markmichealhp@outlook.com>
2020-12-26 20:22:29 +0200

    Added Character fighting interaction and follow player

---
+++ Assets/AnimationController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimationController : MonoBehaviour
+{
+    Animator kidanim;
+    NavMeshAgent agent;
+    public Transform target;
+    public bool follow;
+
+    public Transform Target { get => target; set => target = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = transform;
+        kidanim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(name + " " + follow);
+        if (follow)
+        {
+
+            agent.destination = Target.position;
+            if (agent.remainingDistance < 1f)
+            {
+                StopWalking();
+            }
+            else
+            {
+                Walking();
+            }
+        }
+
+    }
+
+    public void Fighting()
+    {
+        kidanim.SetBool("Idle", false);
+        kidanim.SetBool("Fight", true);
+    }
+
+    public void StopFighting()
+    {
+        kidanim.SetBool("Idle", true);
+        kidanim.SetBool("Fight", false);
+    }
+
+    public void Walking()
+    {
+        agent.destination = Target.position;
+        kidanim.SetBool("Walk", true);
+    }
+
+    public void StopWalking()
+    {
+        agent.destination = transform.position;
+        kidanim.SetBool("Walk", false);
+    }
+
+    public void RotateTowards(Transform target)
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));    // flattens the vector3
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
+    }
+
+
+}
--- Assets/Scripts/FightingInteraction.cs
+++ Assets/Scripts/FightingInteraction.cs
@@ -1,19 +1,84 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FightingInteraction : MonoBehaviour
 {
+    bool playerInZone;
+    public GameObject kidA;
+    AnimationController animControllerA;
+    public GameObject kidB;
+    AnimationController animControllerB;
+
+    public GameObject Player;
+    public bool FollowPlayer;
+    Quaternion playerRot;
+
+    public bool FightingChallengeCompleted;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animControllerA = kidA.GetComponent<AnimationController>();
+        animControllerB = kidB.GetComponent<AnimationController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (FightingChallengeCompleted)
+        {
+            // follow player
+            animControllerA.StopFighting();
+            animControllerB.StopFighting();
+            animControllerA.Target = Player.transform;
+            animControllerA.follow = true;
+            animControllerB.Target = Player.transform;
+            animControllerB.follow = true;
+        }
+        else
+        {
+
+            if (playerInZone)
+            {
+                // pause fighting and look at player
+
+                animControllerA.StopFighting();
+                animControllerA.RotateTowards(Player.transform);
+
+                animControllerB.StopFighting();
+                animControllerB.RotateTowards(Player.transform);
+
+                //kidA.transform.rotation = Quaternion.Slerp(kidA.transform.rotation, Quaternion.LookRotation(lookAtTarget), 5 * Time.deltaTime);
+                //LookAt();
+                //kidA.transform.rotation = Quaternion.Slerp(kidA.transform.rotation, Quaternion.LookRotation(lookAtTarget), 5 * Time.deltaTime);
+
+            }
+            else
+            {
+                // keep fighting
+                animControllerA.Fighting();
+                animControllerA.RotateTowards(kidB.transform);
+
+                animControllerB.Fighting();
+                animControllerB.RotateTowards(kidA.transform);
+                //Debug.Log(transform.parent)
+                //Vector3 lookAtTarget = kidB.transform.position;
+                //Quaternion playerRot = Quaternion.LookRotation(lookAtTarget);
+                //Debug.Log(lookAtTarget);
+                //kidA.transform.rotation = Quaternion.Slerp(kidA.transform.rotation, Quaternion.LookRotation(lookAtTarget), 5 * Time.deltaTime);
+                //kidA.transform.LookAt(lookAtTarget);
+            }
+
+        }
+
+
+    }
+
+    private void LookAt()
+    {
+        throw new NotImplementedException();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -22,6 +87,7 @@ public class FightingInteraction : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             Debug.Log("Collider Player");
+            playerInZone = true;
         }
     }
 
@@ -31,6 +97,7 @@ public class FightingInteraction : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Player Left");
+            playerInZone = false;
         }
     }
 }
