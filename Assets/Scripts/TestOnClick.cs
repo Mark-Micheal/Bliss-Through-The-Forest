adb14e38348338e4efc446fe622ad31d94a68ca5
Caroline <carolineemademil@gmail.com>
2020-12-27 18:29:01 +0200

    Main/Options/Intro Menu

--- Assets/Scripts/TestOnClick.cs
+++ Assets/Scripts/TestOnClick.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TestOnClick : MonoBehaviour
 {
@@ -15,8 +16,8 @@ public class TestOnClick : MonoBehaviour
     {
         
     }
-   public void Test()
+   public void PlayGame()
     {
-        Debug.Log("button here");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
     }
 }
